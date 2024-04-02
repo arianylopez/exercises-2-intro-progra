e0e4890cf56963042125b536af7846bf466e094e
arianylopez <159574397+arianylopez@users.noreply.github.com>
2024-04-01 19:16:59 -0700

    a ver

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -18,7 +18,7 @@ void exercise_2(string s1) {
   // TODO: YOUR CODE HERE
   string word; 
   for(int i=0; i<=s1.size(); ++i){
-    if(s1[i]==''||i==s1.size()){
+    if(s1[i]==' '||i==s1.size()){
       if(!word.empty()){
         cout<<"["<<word<<"]"<<endl;
         word="";
