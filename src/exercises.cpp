6b9f0c556411e26a24bfa615435d4fa9d419d9c7
arianylopez <159574397+arianylopez@users.noreply.github.com>
2024-04-01 19:11:34 -0700

    prueba ejercicio 2

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -16,6 +16,16 @@ void exercise_1(string s1) {
 
 void exercise_2(string s1) {
   // TODO: YOUR CODE HERE
+  for(int i=0; i<=s1.size(); ++i){
+    if(s1[i]==''||i==s1.size()){
+      if(!word.empty()){
+        cout<<"["<<word<<"]"<<endl;
+        word="";
+      }
+    } else{
+      word+=s1[i]
+    }
+  }
 }
 
 void exercise_3(string s1) {
