96d726b7ff7101159c07623ec9bcad0af4ff7743
arianylopez <159574397+arianylopez@users.noreply.github.com>
2024-04-09 20:25:13 -0400

    ejercicios

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -90,6 +90,11 @@ int exercise_6(int n) {
 
 void exercise_7(int n) {
   // TODO: YOUR CODE HERE
+  double suma=0;
+  for(int i=1;n>=i;++i){
+    suma+=pow((-1),(i+1))/i;
+  }
+  cout<<suma<<endl;
 }
 
 void exercise_8(string s) {
