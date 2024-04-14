1d7a776b82a248539a60eada2c70fa968d0842d9
arianylopez <159574397+arianylopez@users.noreply.github.com>
2024-04-14 12:02:40 -0400

    AAAAAA

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -73,7 +73,7 @@ void exercise_5(int n, int k) {
       }
       cout<<i;
       if((i + n - 1)%7==0 || i==k){
-        cout<<" "<<endl;
+        cout<<endl;
       } else{
         cout<<" ";
       }
