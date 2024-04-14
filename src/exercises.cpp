3ed9da3062dcd9bbfd0396fa87c799c0536c51bb
arianylopez <159574397+arianylopez@users.noreply.github.com>
2024-04-14 11:56:05 -0400

    last last last last commit

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -74,8 +74,10 @@ void exercise_5(int n, int k) {
       cout<<i;
       if((i + n - 1)%7==0 || i==k){
         if(i!=k){
+        cout<<endl<<" ";
+      } else{
         cout<<endl;
-        }
+      }
       } else{
         cout<<" ";
       }
