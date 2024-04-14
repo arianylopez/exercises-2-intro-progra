f84dd8b4329cc29faef544ad22822b4069eab5a9
arianylopez <159574397+arianylopez@users.noreply.github.com>
2024-04-14 12:17:02 -0400

    por favor que sea el ultimo

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -73,9 +73,8 @@ void exercise_5(int n, int k) {
       }
       cout<<i;
       if((i + n - 1)%7==0 || i==k){
-        if(i!=k){
-          cout<<" "<<endl;
-        }
+          cout<<" ";
+          cout<<endl;
       } else{
         cout<<" ";
       }
