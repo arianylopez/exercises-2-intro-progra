6b7e3eca594017b9c2d7b2dc499a6e01d1db1831
arianylopez <159574397+arianylopez@users.noreply.github.com>
2024-04-13 23:48:49 -0400

    check

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -140,6 +140,14 @@ void exercise_11() {
 
 void exercise_12() {
   // TODO: YOUR CODE HERE
+  double UI=1;
+  double U=UI;
+  for(int i=1; 10>=i; ++i){
+    int VI=U;
+    double resultado=UI/(i+1);
+    cout<<"U"<<i<<" = "<<UI<<" "<<"V"<<i<<" = "<<VI<<endl;
+    UI=resultado;
+  }
 }
 
 long exercise_13(int n, int k) {
