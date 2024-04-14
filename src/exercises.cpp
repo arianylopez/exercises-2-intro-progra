ecddbae0851ecd73d41520d4908aba3f74760b73
arianylopez <159574397+arianylopez@users.noreply.github.com>
2024-04-14 11:40:01 -0400

    pls

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -65,11 +65,11 @@ void exercise_5(int n, int k) {
   // TODO: YOUR CODE HERE
   int espacios=(n - 1)*3;
   for(int i=0; i<espacios; ++i){
-    cout<<" ";
+    cout<< " ";
   }
     for(int i=1; i<=k; ++i){
       if(i<10){
-        cout<<" ";
+        cout<< " ";
       }
       cout<<i;
       if((i+n - 1)%7==0 || i==k){
